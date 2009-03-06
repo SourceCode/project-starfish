e6107b3a38a8a067fc68a3381ee24f4f22f09b97
SourceCode <rrentfro@gmail.com>
2009-03-06 12:37:12 -0600

    Event Test

---
+++ tests/005-testEvents.php
@@ -0,0 +1,20 @@
+<?php
+  /**
+ * @package starfish
+ * @author Ryan Rentfro, http://www.rentfro.net 
+ * @version 005-testEvents.php, v0.0.1a
+ * @copyright Ryan Rentfro, http://www.rentfro.net
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @category tests
+ */
+
+
+/**
+ * Used to make sure events are working correctly
+ */
+ 
+ require_once('../includes/core.php');
+ 
+ 
+ 
+?>
