65575763d48b3347e99a2710e2fd41a03931b50f
SourceCode <rrentfro@gmail.com>
2009-03-22 01:19:31 -0500

    Initial DataSource and AutoComplete YUI Controllers

---
+++ includes/ajax/controllers/yui/obj.autocomplete.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * @package starfish
+ * @author Ryan Rentfro, http://www.rentfro.net 
+ * @version obj.autocomplete.php, v0.0.1a
+ * @copyright Ryan Rentfro, http://www.rentfro.net
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @category ajaxUI
+ */
+
+/**
+ * @access public
+ * @var object yui auto complete factory
+ * @category objects
+ * 
+ * Use method chaining when calling this object.
+ * 
+ */
+
+class stAutoCompleteFactory extends stYUIFactory
+{ 
+    public $tooltips = array();
+    
+    public function initialize()
+    {
+        $this->namespaces = array('autoCompleteFactory', 'autoCompleteFactory.init');
+        $this->setDefaultData();
+    }      
+    
+    public function getInstance()
+    {
+        if (!isset(self::$instance))
+        {
+            $class = __CLASS__;
+            self::$instance = new $class();
+            self::$instance->initialize(); 
+        }
+        return self::$instance;
+    }
+    
+    public function create($namespace, $context, $container)
+    {
+        $this->dataStore['settings']['namespace'] = $namespace;
+        $this->dataStore['settings']['container'] = $namespace;
+        $this->dataStore['defaults']['context'] = $context;
+        return $this;  
+    }
+    
+    public function render()
+    {
+
+       return true;
+    }
+    
+    public function paint($collapse='')
+    {
+
+    }
+    
+    public function call($call, $value) {
+        if (isset($this->dataStore['calls'][$call]))
+        {
+            $this->dataStore['calls'][$call] = $value;     
+        }
+        return $this;     
+    }
+    
+    private function genConfig()
+    {
+        $defaults = $this->iterateOptionSet($this->dataStore['defaults']);
+        $options = $this->iterateOptionSet($this->dataStore['options']);
+        return (!empty($options)) ? $defaults . ', ' . $options:$defaults;
+    }
+    
+    public function setData($source, $type='')
+    {
+        if ($type=='local' || $type=='')
+        {
+         
+         
+        } elseif($type=='local') {
+              
+        } else {
+            return false;
+        }
+        return $this;          
+    }
+    
+    public function setSchema($schema)
+    {
+        
+    }
+    
+    protected function setDefaultData()
+    {
+        $this->dataStore = array('namespace', 'instantiate', 'settings', 'tplVals', 'defaults', 'options', 'target', 'return', 'calls');
+        
+        $this->dataStore['instantiate'] = '
+            // Define Datasource
+            var oDS = new {dataSource};
+            // Define Schema
+            oDS.responseSchema = {{schema}};
+
+            // Instantiate the AutoComplete
+            YAHOO.autoCompleteFactory.{name} = new YAHOO.widget.AutoComplete("{field}", "{container}", oDS);';
+        
+        $this->dataStore['return'] = 'return {
+            oDS: oDS,
+            YAHOO.autoCompleteFactory.{name}: YAHOO.autoCompleteFactory.{name}
+            };';
+        
+        $this->dataStore['namespace'] = 'YAHOO.namespace("{namespace}");';
+        
+        $this->dataStore['tplVals']['settings'] = array('{name}'); 
+        
+        $this->dataStore['defaults']['context'] = '';
+        
+        $this->dataStore['options']['showdelay'] = '';
+
+        $this->dataStore['calls']['prehighlightClassName'] = 'yui-ac-prehighlight';
+        
+        $this->dataStore['calls']['useShadow'] = true;
+        
+        return true;
+    }
+}
---
+++ includes/ajax/controllers/yui/obj.datasource.php
@@ -0,0 +1,150 @@
+<?php
+
+/**
+ * @package starfish
+ * @author Ryan Rentfro, http://www.rentfro.net 
+ * @version obj.datasource.php, v0.0.1a
+ * @copyright Ryan Rentfro, http://www.rentfro.net
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @category ajaxUI
+ */
+
+/**
+ * @access public
+ * @var object yui dataSource factory
+ * @category objects
+ * 
+ * Use method chaining when calling this object.
+ * 
+ */
+
+class stAutoCompleteFactory extends stYUIFactory
+{ 
+    public $dataSource = array();
+    private $schemaSet = false;
+    
+    public function initialize()
+    {
+        $this->namespaces = array('dataSourceFactory', 'dataSourceFactory.init');
+        $this->setDefaultData();
+    }      
+    
+    public function getInstance()
+    {
+        if (!isset(self::$instance))
+        {
+            $class = __CLASS__;
+            self::$instance = new $class();
+            self::$instance->initialize(); 
+        }
+        return self::$instance;
+    }
+    
+    public function create($namespace, $data, $type, $method='')
+    {
+        $this->dataStore['settings']['namespace'] = $namespace;
+        $this->dataStore['settings']['data'] = $data;
+        $this->dataStore['settings']['type'] = $type;
+        $this->dataStore['settings']['method'] = $type;
+        return $this;  
+    }
+    
+    public function render()
+    { 
+        if ($this->schemaSet === false) return false;
+        $callSet = '';
+        $name = '';
+        $this->buffer = 'YAHOO.dataSourceFactory.init.{name} = function() {' . "\n" . $this->dataStore['instantiate'] . "\n" . '{schema}' . "\n\n" . '{calls}' . "\n\n}\n\n"  . 'YAHOO.util.Event.onDOMReady(YAHOO.dataSourceFactory.init.{name});';
+        $this->buffer = str_replace('{name}', $this->dataStore['settings']['namespace'], $this->buffer);
+        $this->buffer = str_replace('{type}', $this->dataStore['settings']['type'], $this->buffer);
+        $this->buffer = str_replace('{source}', $this->dataStore['settings']['data'], $this->buffer);
+        foreach($this->dataStore['calls'] as $function => $arg)
+        {
+            if (!empty($arg))
+            {
+                $call =  'YAHOO.dataSourceFactory.init.{name}.' . $function . ' = ';
+                 if ((is_string($arg) || is_int($arg)) && !is_bool($arg))
+                {
+                    if (!empty($arg)) $call .= ((is_int($arg)) ?  $arg:'"' . $arg . '"');
+                } elseif (is_bool($arg)) 
+                {
+                    $call .= ($arg) ? 'true':'false';
+                }
+                $callSet .= $call . "\n";
+            }   
+        }
+        $this->buffer = str_replace('{calls}', $callSet, $this->buffer);
+        $callSet = '';
+        $this->buffer = str_replace('{schema}', $this->dataStore['settings']['schema'], $this->buffer);
+        $this->dataStore['settings']['schema'] = '';
+        $this->buffer = str_replace('{dataClass}', $this->dataStore['settings']['dataClass'], $this->buffer);
+        $name = $this->dataStore['settings']['namespace'];
+        $this->dataSource[$name] = $this->buffer;
+        $this->buffer = '';
+        $this->setDefaultData();
+       return true;
+    }
+    
+    public function paint($collapse='')
+    {
+        $tmpBuffer = '';
+        $nameSpaces = '';
+        $namespace = '';
+        $data = '';
+        $dialogs = '';
+        if (is_array($this->namespaces) && is_array($this->dataSource) && isset($this->dataStore['namespace']))
+        {
+            foreach($this->namespaces as $namespace)
+            {
+                $nameSpaces .= str_replace('{namespace}', $namespace, $this->dataStore['namespace']) . "\n";                  
+            }
+            
+            $dataSources = implode("\n", $this->dataSource);         
+            
+            $finalCode = $nameSpaces . "\n" . $dialogs;
+            $dialogs = '';
+            if ($collapse === true) {
+               $finalCode = trim(preg_replace('/\s+/',' ', $finalCode));
+            } 
+            return $finalCode;
+        } else {
+            return false;
+        }  
+    }
+    
+    public function setSchema($schema, $dataClass)
+    {
+        $this->schemaSet = true;
+        $this->dataStore['settings']['schema'] = $schema;
+        $this->dataStore['settings']['dataClass'] = $dataClass;   
+    }
+    
+    private function genConfig()
+    {
+        $defaults = $this->iterateOptionSet($this->dataStore['defaults']);
+        $options = $this->iterateOptionSet($this->dataStore['options']);
+        return (!empty($options)) ? $defaults . ', ' . $options:$defaults;
+    }
+    
+    protected function setDefaultData()
+    {
+        $this->dataStore = array('namespace', 'instantiate', 'settings', 'tplVals', 'defaults', 'options', 'calls');
+        
+        $this->dataStore['instantiate'] = 'YAHOO.dataSourceFactory.{name} = new YAHOO.util.{type}({source});';
+
+        $this->dataStore['settings']['schema'] = '';
+        $this->dataStore['settings']['dataClass'] = '';
+        
+        $this->dataStore['defaults']['connXhrMode'] = ''; 
+        
+        $this->dataStore['calls']['setType'] = 'YAHOO.util.{type}.{dataClass}';
+        $this->dataStore['calls']['responseType'] = '';
+        $this->dataStore['calls']['responseSchema'] = '';
+        $this->dataStore['calls']['setInterval'] = '';
+        $this->dataStore['calls']['clearInterval'] = '';
+        
+        $this->schemaSet = false;
+        
+        return true;
+    }
+}
